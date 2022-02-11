fe66b038851619242db27b9c4477b2c4a500d360
patipatfwg <48121046+patipatfwg@users.noreply.github.com>
2022-02-11 14:58:20 +0700

    Update registerMenu.php

--- menu/registerMenu.php
+++ menu/registerMenu.php
@@ -3,7 +3,10 @@
 error_reporting(-1);
 ini_set('display_errors', 'On');
 
-// include($_SERVER['DOCUMENT_ROOT'] . "/define_Global.php");
+include_once("zApiFunction.php");
+include_once("zMenuFunction.php");
+include_once($_SERVER['DOCUMENT_ROOT'] . "/define_Global.php");
+
 
 $GLOBALS['COMPANY_URL'] =  COMPANY_URL;
 $GLOBALS['REGISTER_URL'] =   REGISTER_URL;
