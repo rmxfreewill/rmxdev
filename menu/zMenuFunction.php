1934cf056a6bc9f73cce4292082cdbc2df71f5ac
patipatfwg <48121046+patipatfwg@users.noreply.github.com>
2022-02-14 15:15:22 +0700

    1515

--- menu/zMenuFunction.php
+++ menu/zMenuFunction.php
@@ -3,6 +3,7 @@
 error_reporting(-1);
 ini_set('display_errors', 'On');
 
+include_once("zApiFunction.php");
 include_once($_SERVER['DOCUMENT_ROOT'] . "/define_Global.php");
 
 $GLOBALS['COMPANY_URL'] =  COMPANY_URL;
