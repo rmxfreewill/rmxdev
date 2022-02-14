6cc5a9fdf69a197b115a84802cff88f78f45318c
patipatfwg <48121046+patipatfwg@users.noreply.github.com>
2022-02-14 14:52:24 +0700

    Update ticketMenu.php

--- menu/ticketMenu.php
+++ menu/ticketMenu.php
@@ -3,7 +3,7 @@
 error_reporting(-1);
 ini_set('display_errors', 'On');
 
-include_once("/zMenuFunction.php");
+include_once("zMenuFunction.php");
 
 $getDataFromUrl = getDataFromUrlv2();
 $status = $getDataFromUrl->status;
