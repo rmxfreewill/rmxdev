b85e2c4afda347aeab4abe7916de7ef7000dbe41
patipatfwg <48121046+patipatfwg@users.noreply.github.com>
2022-02-14 14:15:56 +0700

    1415

--- menu/ticketMenu.php
+++ menu/ticketMenu.php
@@ -20,67 +20,49 @@ $status = $getDataFromUrl->status;
 //     $sFlag = $getDataFromDatabase->sFlag;
 // }
 
+
+// if ($sFlag != '0') {
+// $getTicketFromDatabase = getTicketFromDatabase($getDataFromUrl, $getDataFromDatabase);
+// showTicketList($getTicketFromDatabase);
+// } else {
+// echo $notFound;
+// echo "<p>";
+// echo  $getDataFromDatabase->status;
+// }
+
 ?>
 
-<!DOCTYPE HTML>
-<html>
-
-<head>
-    <title>RMX LINE OFFICIAL</title>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta http-equiv="content-language" content="en-th">
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-</head>
-
-<body>
-    <h1>Ticket</h1>
-    <?php
-    // if ($sFlag != '0') {
-        // $getTicketFromDatabase = getTicketFromDatabase($getDataFromUrl, $getDataFromDatabase);
-        // showTicketList($getTicketFromDatabase);
-    // } else {
-        // echo $notFound;
-        // echo "<p>";
-        // echo  $getDataFromDatabase->status;
+<script>
+    // function openPage(pageName, elmnt, color) {
+    //     var i, tabcontent, tablinks;
+    //     tabcontent = document.getElementsByClassName("tabcontent");
+    //     for (i = 0; i < tabcontent.length; i++) {
+    //         tabcontent[i].style.display = "none";
+    //     }
+    //     tablinks = document.getElementsByClassName("tablink");
+    //     for (i = 0; i < tablinks.length; i++) {
+    //         tablinks[i].style.backgroundColor = "";
+    //     }
+    //     document.getElementById(pageName).style.display = "block";
+    //     elmnt.style.backgroundColor = color;
     // }
-    ?>
-    <script>
 
+    // function fillTicketData(tableName, asCol, asData) {
+    //     var table = document.getElementById(tableName);
+    //     if (table) {
+    //         var sHtml = "";
+    //         var nRLen = asData.length;
+    //         for (var r = 0; r < nRLen; r++) {
+    //             var sC = asCol[r];
+    //             var sD = asData[r];
+    //             sHtml = sHtml + "<tr><th>" + sC + "</th><td class='textLeft'>" + sD + "</td></tr>";
+    //         }
+    //         table.innerHTML = sHtml;
+    //     }
+    // }
 
-        // function openPage(pageName, elmnt, color) {
-        //     var i, tabcontent, tablinks;
-        //     tabcontent = document.getElementsByClassName("tabcontent");
-        //     for (i = 0; i < tabcontent.length; i++) {
-        //         tabcontent[i].style.display = "none";
-        //     }
-        //     tablinks = document.getElementsByClassName("tablink");
-        //     for (i = 0; i < tablinks.length; i++) {
-        //         tablinks[i].style.backgroundColor = "";
-        //     }
-        //     document.getElementById(pageName).style.display = "block";
-        //     elmnt.style.backgroundColor = color;
-        // }
-
-        // function fillTicketData(tableName, asCol, asData) {
-        //     var table = document.getElementById(tableName);
-        //     if (table) {
-        //         var sHtml = "";
-        //         var nRLen = asData.length;
-        //         for (var r = 0; r < nRLen; r++) {
-        //             var sC = asCol[r];
-        //             var sD = asData[r];
-        //             sHtml = sHtml + "<tr><th>" + sC + "</th><td class='textLeft'>" + sD + "</td></tr>";
-        //         }
-        //         table.innerHTML = sHtml;
-        //     }
-        // }
-
-        $(function() {
-            alert('Hi');
-            $(".loader").hide();
-        });
-    </script>
-</body>
-
-</html>
+    $(function() {
+        alert('Hi');
+        $(".loader").hide();
+    });
+</script>
