54cd8322025b5779fe7ad9037ed7a6a1e6eaea36
patipatfwg <48121046+patipatfwg@users.noreply.github.com>
2022-02-17 17:08:19 +0700

    1708

--- menu/registerMenu.php
+++ menu/registerMenu.php
@@ -29,7 +29,7 @@ if ($sFlag == '4') {
 }
 
 ?>
-<div class="bg-white rounded-20 mb-3 mt-3">
+<div class="mb-3 mt-3 bg-white shadow-sm p-3 mb-5 bg-white rounded">
     <div class="col-12 p-3">
         <h3>Register</h3>
     </div>
