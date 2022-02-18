2d96b615ba39471646d47c3f2abe89489cd7c4f6
patipatfwg <48121046+patipatfwg@users.noreply.github.com>
2022-02-18 11:12:07 +0700

    Update profileMenu.php

--- menu/profileMenu.php
+++ menu/profileMenu.php
@@ -30,14 +30,12 @@ try {
             <h2>Profile</h2>
         </div>
         <div class="col-12">
-            <form>
-                <div class="row mb-3">
-                    <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
-                    <div class="col-sm-10">
-                        <input type="email" class="form-control" id="inputEmail3">
-                    </div>
+            <div class="row mb-3">
+                <label for="colFormLabelSm" class="col-sm-2 col-form-label col-form-label-sm">Email</label>
+                <div class="col-sm-10">
+                    <input type="email" class="form-control form-control-sm" id="colFormLabelSm" placeholder="col-form-label-sm">
                 </div>
-            </form>
+            </div>
         </div>
     </div>
 </div>
