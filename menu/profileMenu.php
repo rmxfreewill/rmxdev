7a27552760a155875dbe063589b2cc9d92d06705
patipatfwg <48121046+patipatfwg@users.noreply.github.com>
2022-02-18 11:10:27 +0700

    Update profileMenu.php

--- menu/profileMenu.php
+++ menu/profileMenu.php
@@ -30,7 +30,7 @@ try {
             <h2>Profile</h2>
         </div>
         <div class="col-12">
-            <form class="row g-3">
+            <form>
                 <div class="row mb-3">
                     <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
                     <div class="col-sm-10">
