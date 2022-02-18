d10501662002f5ed34414967cb988e526f78fa07
patipatfwg <48121046+patipatfwg@users.noreply.github.com>
2022-02-18 11:14:48 +0700

    Update profileMenu.php

--- menu/profileMenu.php
+++ menu/profileMenu.php
@@ -31,10 +31,7 @@ try {
         </div>
         <div class="col-12">
             <div class="row mb-3">
-                <label for="colFormLabelSm" class="col-sm-2 col-form-label col-form-label-sm">Email</label>
-                <div class="col-sm-10">
-                    <input type="email" class="form-control form-control-sm" id="colFormLabelSm" placeholder="col-form-label-sm">
-                </div>
+                <input type="input" class="form-control form-control-sm" id="colFormLabelSm" value="<?php echo $nameText; ?>" readonly>
             </div>
         </div>
     </div>
