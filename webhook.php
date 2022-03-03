0a4d2c88481450570d08631dd10e52a1055c8cba
patipatfwg <48121046+patipatfwg@users.noreply.github.com>
2022-03-03 22:28:55 +0700

    2228

--- webhook.php
+++ webhook.php
@@ -5,13 +5,35 @@ ini_set('display_errors', 'On');
 
 include_once("define_Global.php");
 include_once("rmxWebhookFunction.php");
-include_once("menu/zApiFunction.php");
 
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: GET,POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+function rmxGetDataLiff($menu, $LineId)
+{
+    $rmx_api_url = RMX_SERVER_API_URL;
+    $param_menu = "?menu=$menu&lineid=$LineId";
+    $url = $rmx_api_url  . $param_menu;
+    $headers = ["Authorization: Bearer " . BEARER_TOKEN];
+
+    try {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+        curl_setopt($ch, CURLOPT_POSTFIELDS, array());
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, TRUE);
+        $data = curl_exec($ch);
+        curl_close($ch);
+        return  $data;
+    } catch (Exception $ex) {
+        print('Error rmxProfileLiff: ' . $ex);
+    }
+}
 
 function ticketDetailRowLayout($title, $val)
 {
