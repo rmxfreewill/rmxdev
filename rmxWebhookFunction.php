356133f1912dc5e9f21527be7d5d94737dc81eea
patipatfwg <48121046+patipatfwg@users.noreply.github.com>
2022-03-03 23:10:40 +0700

    Update rmxWebhookFunction.php

--- rmxWebhookFunction.php
+++ rmxWebhookFunction.php
@@ -48,7 +48,8 @@ function rmxChangeRegisterRichMenu($LINEID)
 
 function sendMessage($replyJson)
 {
-    $url = "https://api.line.me/v2/bot/message/multicast";
+    // $url = "https://api.line.me/v2/bot/message/multicast";
+    $url = "https://api.line.me/v2/bot/message/push";
 
     $sendInfo['URL'] = $url;
     $sendInfo['AccessToken'] = $GLOBALS['BEARER_TOKEN'];
